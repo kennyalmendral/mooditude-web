d381631b6bfc07e819d13810cd47c3eb7a52b025
Justy <justyramos.webdev@gmail.com>
2021-11-25 00:38:51 +0800

    CSS Fix

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -170,6 +170,7 @@ export default function SignUp(props) {
     } else {
       setError('Passwords do not match.')
       setIsSigningUp(false)
+      setBtnDisabled(password, passwordConfirmation, isPrivacyPolicyChecked)
     }
   }
 
